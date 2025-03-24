0cb84648cb251429686b506a37118895f5bc908a
Shafin5714 <Shafin5714@gmail.com>
2025-03-24 12:18:45 +0600

    chat navbar design

--- client/src/components/PrivateLayout.tsx
+++ client/src/components/PrivateLayout.tsx
@@ -1,11 +1,27 @@
 import { ModeToggle } from "./mode-toggle";
 import { Outlet } from "react-router";
+import { Button } from "./ui/button";
+import { LogOut, User, MessageSquare } from "lucide-react";
 
 export default function PrivateLayout() {
   return (
     <div className="h-screen">
-      <div className="px-4 py-2 h-14">
-        <ModeToggle />
+      <div className="px-5 py-2 h-14 flex justify-between items-center">
+        <Button variant="ghost" className="text-lg">
+          <MessageSquare />
+          Chat Bridge
+        </Button>
+        <div className="flex items-center gap-4">
+          <ModeToggle />
+          <Button variant="outline">
+            <User />
+            Profile
+          </Button>
+          <Button variant="destructive">
+            <LogOut />
+            Logout
+          </Button>
+        </div>
       </div>
       <div className="h-[calc(100vh-3.5rem)] bg-red-500">
         <Outlet />
