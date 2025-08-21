eb808f40d5ccc0bd1f6444f979112d2fb0d9d1bd
Shafin5714 <Shafin5714@gmail.com>
2025-08-21 14:41:54 +0600

    add: shared media

--- client/src/features/chat/components/shared-media.tsx
+++ client/src/features/chat/components/shared-media.tsx
@@ -1,12 +1,19 @@
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
+import { useAppSelector } from "@/store";
 
 type Props = {
   mobileView: string;
 };
 
 function SharedMedia({ mobileView }: Props) {
+  const { messages } = useAppSelector((state) => state.message);
+
+  const images = messages
+    .filter((message) => message.image)
+    .map((images) => images.image);
+
   return (
     <Card
       className={`w-full lg:w-80 ${
@@ -20,90 +27,19 @@ function SharedMedia({ mobileView }: Props) {
       <CardContent>
         <ScrollArea className="h-[calc(100vh-20rem)] lg:h-[calc(100vh-16rem)]">
           <div className="grid grid-cols-2 gap-4">
-            <div>
-              <img
-                src={"https://placehold.co/600x400"}
-                alt={`Shared media`}
-                className="w-full h-auto rounded-lg"
-              />
-            </div>
-            <div>
-              <img
-                src={"https://placehold.co/600x400"}
-                alt={`Shared media`}
-                className="w-full h-auto rounded-lg"
-              />
-            </div>
-            <div>
-              <img
-                src={"https://placehold.co/600x400"}
-                alt={`Shared media`}
-                className="w-full h-auto rounded-lg"
-              />
-            </div>
-            <div>
-              <img
-                src={"https://placehold.co/600x400"}
-                alt={`Shared media`}
-                className="w-full h-auto rounded-lg"
-              />
-            </div>
-            <div>
-              <img
-                src={"https://placehold.co/600x400"}
-                alt={`Shared media`}
-                className="w-full h-auto rounded-lg"
-              />
-            </div>
-            <div>
-              <img
-                src={"https://placehold.co/600x400"}
-                alt={`Shared media`}
-                className="w-full h-auto rounded-lg"
-              />
-            </div>
-            <div>
-              <img
-                src={"https://placehold.co/600x400"}
-                alt={`Shared media`}
-                className="w-full h-auto rounded-lg"
-              />
-            </div>
-            <div>
-              <img
-                src={"https://placehold.co/600x400"}
-                alt={`Shared media`}
-                className="w-full h-auto rounded-lg"
-              />
-            </div>
-            <div>
-              <img
-                src={"https://placehold.co/600x400"}
-                alt={`Shared media`}
-                className="w-full h-auto rounded-lg"
-              />
-            </div>
-            <div>
-              <img
-                src={"https://placehold.co/600x400"}
-                alt={`Shared media`}
-                className="w-full h-auto rounded-lg"
-              />
-            </div>
-            <div>
-              <img
-                src={"https://placehold.co/600x400"}
-                alt={`Shared media`}
-                className="w-full h-auto rounded-lg"
-              />
-            </div>
-            <div>
-              <img
-                src={"https://placehold.co/600x400"}
-                alt={`Shared media`}
-                className="w-full h-auto rounded-lg"
-              />
-            </div>
+            {images.length ? (
+              images.map((image) => (
+                <div key={image}>
+                  <img
+                    src={image}
+                    alt={`Shared media`}
+                    className="h-auto w-full rounded-lg"
+                  />
+                </div>
+              ))
+            ) : (
+              <h2 className="text-gray-600">No Shared Media</h2>
+            )}
           </div>
         </ScrollArea>
       </CardContent>
