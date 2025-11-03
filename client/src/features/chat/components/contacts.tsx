6b3071d4da622ba18ff8847d91ecaedcb98e5549
Shafin5714 <Shafin5714@gmail.com>
2025-11-03 15:31:33 +0600

    contacts ui updated

--- client/src/features/chat/components/contacts.tsx
+++ client/src/features/chat/components/contacts.tsx
@@ -1,5 +1,4 @@
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { Avatar } from "@radix-ui/react-avatar";
+import { Card, CardHeader } from "@/components/ui/card";
 import { useGetUsersQuery } from "@/store/api/messageApi";
 
 import { ScrollArea } from "@/components/ui/scroll-area";
@@ -58,63 +57,97 @@ export default function Contacts({ mobileView }: Props) {
             </div>
           </h2>
         </CardHeader>
-        <Separator className="mb-4" />
-        <CardContent>
+        <Separator />
+        <div>
           {/* Search Input */}
-          <div className="relative mb-4">
-            <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-gray-400" />
-            <input
-              type="text"
-              placeholder="Search contacts..."
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-              className="w-full rounded-lg border border-gray-300 bg-background py-2 pl-10 pr-4 text-sm focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
+          <div className="p-3">
+            <div className="relative">
+              <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-gray-400" />
+              <input
+                type="text"
+                placeholder="Search contacts..."
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                className="w-full rounded-lg border border-gray-300 bg-background py-2 pl-10 pr-4 text-sm focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
           </div>
-          <ScrollArea className="h-[calc(100vh-22rem)] lg:h-[calc(100vh-18rem)]">
-            <div className="space-y-4">
-              {/* Filter users based on search term */}
-              {userList
-                .filter((user) =>
-                  user.name.toLowerCase().includes(searchTerm.toLowerCase()),
-                )
-                .map((user) => (
-                  <div
-                    key={user._id}
-                    onClick={() =>
-                      dispatch(userSlice.actions.setSelectedUser(user))
-                    }
-                    className={cn(
-                      "flex cursor-pointer items-center space-x-4 rounded-lg p-2 transition-colors duration-200",
-                      selectedUser?._id === user._id
-                        ? "bg-slate-800 text-white"
-                        : "bg-secondary",
-                      "hover:bg-slate-800 hover:text-white",
-                    )}
-                  >
-                    <Avatar
+
+          <Separator />
+          <div className="p-3">
+            <ScrollArea className="h-[calc(100vh-22rem)] lg:h-[calc(100vh-18rem)]">
+              <div className="space-y-2">
+                {/* Filter users based on search term */}
+                {userList
+                  .filter((user) =>
+                    user.name.toLowerCase().includes(searchTerm.toLowerCase()),
+                  )
+                  .map((user) => (
+                    <div
+                      key={user._id}
+                      onClick={() =>
+                        dispatch(userSlice.actions.setSelectedUser(user))
+                      }
                       className={cn(
-                        "rounded-full border-2 border-blue-500 p-2 hover:border-gray-400",
+                        "flex cursor-pointer items-center space-x-4 rounded-lg p-2 transition-colors duration-200",
+                        selectedUser?._id === user._id
+                          ? "bg-blue-500/10"
+                          : "hover:bg-gray-50",
                       )}
                     >
-                      <User size={32} strokeWidth={2} />
-                    </Avatar>
+                      <div className="flex w-full justify-between px-2 py-1">
+                        <div className="flex gap-3">
+                          <div className="relative">
+                            <div
+                              className={cn(
+                                "rounded-full border-2 border-blue-500 p-2 hover:border-gray-400",
+                              )}
+                            >
+                              <User size={28} strokeWidth={2} />
+                            </div>
+                            <p className="absolute bottom-0 right-0 text-xs text-gray-500">
+                              {onlineUsers.includes(user._id) ? (
+                                <Circle
+                                  fill="green"
+                                  size={12}
+                                  strokeWidth={0}
+                                />
+                              ) : (
+                                <Circle fill="red" size={12} strokeWidth={0} />
+                              )}
+                            </p>
+                          </div>
 
-                    <div className="flex items-center gap-1">
-                      <p className="text-sm font-medium">{user.name}</p>
-                      <p className="text-xs text-gray-500">
-                        {onlineUsers.includes(user._id) ? (
-                          <Circle fill="green" size={12} strokeWidth={0} />
-                        ) : (
-                          <Circle fill="red" size={12} strokeWidth={0} />
-                        )}
-                      </p>
+                          <div className="flex flex-col gap-1">
+                            <p className="line-clamp-1 text-base font-medium leading-normal text-gray-900">
+                              {user.name}
+                            </p>
+                            <p className="leading-norma line-clamp-1 text-sm font-medium font-normal text-gray-500">
+                              Last message
+                            </p>
+                          </div>
+                        </div>
+
+                        <div>
+                          <div className="shrink-0 text-right">
+                            <p className="text-xs font-normal leading-normal text-gray-500">
+                              10:23 PM
+                            </p>
+
+                            <div className="mt-1 flex justify-end">
+                              <div className="flex h-6 w-6 items-center justify-center rounded-full bg-blue-500 text-xs font-semibold text-white">
+                                2
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                ))}
-            </div>
-          </ScrollArea>
-        </CardContent>
+                  ))}
+              </div>
+            </ScrollArea>
+          </div>
+        </div>
       </Card>
     </div>
   );
